ea15122a74ce2e2c487548dc823b0ceb512528bc
itomsawyer <tomsawyer126@gmail.com>
2023-03-07 10:28:13 +0800

    condition build

--- build_tongsuo.go
+++ build_tongsuo.go
@@ -22,5 +22,5 @@ package openssl
 // #cgo darwin CFLAGS: -I/opt/tongsuo/include -Wno-deprecated-declarations
 // #cgo darwin LDFLAGS: -L/opt/tongsuo/lib -lssl -lcrypto
 // #cgo windows CFLAGS: -DWIN32_LEAN_AND_MEAN
-// #cgo windows pkg-config: --static libssl libcrypto
+// #cgo windows pkg-config: libssl libcrypto
 import "C"
